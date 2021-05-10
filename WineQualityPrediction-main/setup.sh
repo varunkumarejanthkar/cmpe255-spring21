63fcd5ef947e0387205bbd57c90bcbdc4dc1f01f
varunkumarejanthkar <varunkumarejanthkar@gmail.com>
2021-05-10 16:48:57 -0700

    Added setup.sh file and fixed bugs

--- WineQualityPrediction-main/setup.sh
+++ WineQualityPrediction-main/setup.sh
@@ -1,12 +1,8 @@
-
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-
+mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
